900be9eed05426eb6ed563ac73a87fcf9b9754c7
xm_1898 <xm_1898@163.com>
2018-03-20 20:53:05 +0800

    完成搜索框、banner、分类、头条模块

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by SHAW on 2018/3/20.
+ */
+$(function () {
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        autoplay: 2000,
+        autoplayDisableOnInteraction : false,
+        // 如果需要分页器
+        pagination: '.swiper-pagination'
+    })
+})
+
